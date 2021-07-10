c0a335c3da6020dafb3d1997809a6c5cf7826dca
0x0is1 <0x0is1@protonmail.com>
2021-07-10 18:43:37 +0530

    [+] Added data parsing module

--- libm2v1/libm2v1.py
+++ libm2v1/libm2v1.py
@@ -15,8 +15,7 @@ class StdOutListener(StreamListener):
 
     def on_data(self, data):
         psdata=json.loads(data)
-        if psdata['id'] != 96900937:
-          return
+        if psdata['user']['id_str']!=ID: return
         with open('cache.json', 'w') as f:
             json.dump(psdata, f)
         print('data recieved')
@@ -24,19 +23,32 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print(status)
 
+def parse_data(raw_data):
+  timec=raw_data['created_at']
+  text=raw_data['text'].split('https://t.co/')[0]
+  username=raw_data['user']['name']
+  prof_thumb=raw_data['user']['profile_image_url_https']
+  urls, images=[], []
+  for i in raw_data['entities']['urls']:
+    urls.append(i['expanded_url'])
+  for j in raw_data['entities']['media']:
+    images.append(j['media_url_https'])
+  return timec, text, username, prof_thumb, urls, images
+
 if __name__ == '__main__':
     api = tweepy.API(auth)
   
     # the screen name of the user
-    screen_name = "ndtvfeed"
+    screen_name = "0x0is1"
   
     # fetching the user
     user = api.get_user(screen_name)
   
     # fetching the ID
+    global ID
     ID = user.id_str
     print(ID)
     listener = StdOutListener()
     twitterStream = Stream(auth, listener)
     twitterStream.filter(follow=[ID])
-  
+    
