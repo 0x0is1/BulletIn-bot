0f4fe4bb497e2c4961d3621775d3f017ef50640a
0x0is1 <0x0is1@protonmail.com>
2021-07-11 17:36:47 +0530

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,6 @@
+import os
+CONSUMER_KEY=os.environ.get('CK')
+CONSUMER_SECRET=os.environ.get('CS')
+ACCESS_KEY=os.environ.get('AK')
+ACCESS_SECRET=os.environ.get('AS')
+BOT_TOKEN=os.environ.get('EXPERIMENTAL_BOT_TOKEN')
