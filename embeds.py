af72bfe018f3954b818c6bb0c852a1fd87fe91d6
0x0is1 <0x0is1@protonmail.com>
2021-07-12 21:22:06 +0530

    Update embeds.py

--- embeds.py
+++ embeds.py
@@ -1,16 +1,14 @@
 import discord
 
 def tweet_embed(data):
-  embed = discord.Embed(title=data[2], color=0x11806a)
-  embed.set_thumbnail(url=data[3])
-  embed.add_field(name='Message: ', value=data[1], inline=False)
+  embed = discord.Embed(title="Twitter", color=0x11806a)
   v=''
   imgs=''
   if len(data[4]) >= 1:
-    for i, j in enumerate(data[4]):
-      v+='[Read more[{0}]]({1})\n'.format(i, j)
-    embed.add_field(name='Visit: ', value=v, inline=False)
-    
+    for i in data[4]:
+      v+='[Read more]({0})\n'.format(i)
+  message=data[1] + ' ' + v
+  embed.add_field(name=data[2], value=message, inline=False)
   if len(data[5]) >= 1:
     embed.set_image(url=data[5][0])
     data[5].pop(0)
@@ -19,5 +17,5 @@ def tweet_embed(data):
         imgs+='[Image[{0}]]({1})\n'.format(i, j)
       embed.add_field(name='More images: ', value=imgs, inline=False)
   
-  embed.set_footer(text=data[0])
+  embed.set_footer(text=data[0], icon_url=data[3])
   return embed
