017aca115c30dd332028446ffe28b1ef79eae17c
0x0is1 <0x0is1@protonmail.com>
2021-07-14 21:38:13 +0530

    Update libm2v1.py

--- libm2v1/libm2v1.py
+++ libm2v1/libm2v1.py
@@ -1,19 +1,25 @@
 import re
 def parse_data(raw_data):
   timec=raw_data['created_at'].replace(' +0000', '')
-  text=raw_data['text']
-  urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+', text)
-  for i in urls:
-    text=text.replace(i, '\n[Visit]({0})'.format(i))
   username=raw_data['user']['name']
   prof_thumb=raw_data['user']['profile_image_url_https']
-  images=[]
   
+  try:
+    raw_data=raw_data['extended_tweet']
+    text=raw_data['full_text']
+  except KeyError:
+    text=raw_data['text']
+  images=[]
   try:
     for j in raw_data['entities']['media']:
       images.append(j['media_url_https'])
   except KeyError:pass
-
+  urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+', text)
+  for i in range((len(urls) - len(images))):
+    text=text.replace(urls[i], '[Visit]({0})'.format(urls[i]))
+    urls.pop(i)
+  for j in urls:
+    text=text.replace(j, '')
   return timec, text, username, prof_thumb, images
 
 def fetch_user(username, api):
