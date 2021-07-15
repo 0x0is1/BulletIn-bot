a73fad2a21976ea51fadc53483c808429fd72c21
0x0is1 <0x0is1@protonmail.com>
2021-07-15 17:18:47 +0530

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,26 @@
 import os
-CONSUMER_KEY=os.environ.get('CK')
-CONSUMER_SECRET=os.environ.get('CS')
-ACCESS_KEY=os.environ.get('AK')
-ACCESS_SECRET=os.environ.get('AS')
-BOT_TOKEN=os.environ.get('EXPERIMENTAL_BOT_TOKEN')
+
+CONSUMER_KEY = os.environ.get('CK')
+CONSUMER_SECRET = os.environ.get('CS')
+ACCESS_KEY = os.environ.get('AK')
+ACCESS_SECRET = os.environ.get('AS')
+BOT_TOKEN = os.environ.get('EXPERIMENTAL_BOT_TOKEN')
+follow_handles = [
+    'ndtvfeed', 
+    'DRDO_India',
+    'isro',
+    'ndtv',
+    'DeportesCuatro',
+    'ferrancorreas', 'lesportiuCAT', 'mcgrathmike', 'FlorentTorchut',
+    'santiovalle',
+    'gbsans', 
+    'LivEchoLFC', 
+    'sport', 
+    'moillorens',
+    'samuelmarsden', 
+    'ffpolo', 
+    'Alfremartinezz','martinezferran',
+    'sergisoleMD',
+    'Benayadachraf',
+    'fansjavimiguel', 'EsportsRAC1'
+]
