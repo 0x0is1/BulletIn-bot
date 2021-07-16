c6b3a89f2b677e21023c21ea208cac3417da7808
0x0is1 <0x0is1@protonmail.com>
2021-07-16 15:40:22 +0530

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,26 +1,9 @@
-import os
+import os, json
 
 CONSUMER_KEY = os.environ.get('CK')
 CONSUMER_SECRET = os.environ.get('CS')
 ACCESS_KEY = os.environ.get('AK')
 ACCESS_SECRET = os.environ.get('AS')
 BOT_TOKEN = os.environ.get('EXPERIMENTAL_BOT_TOKEN')
-follow_handles = [
-    'ndtvfeed', 
-    'DRDO_India',
-    'isro',
-    'ndtv',
-    'DeportesCuatro',
-    'ferrancorreas', 'lesportiuCAT', 'mcgrathmike', 'FlorentTorchut',
-    'santiovalle',
-    'gbsans', 
-    'LivEchoLFC', 
-    'sport', 
-    'moillorens',
-    'samuelmarsden', 
-    'ffpolo', 
-    'Alfremartinezz','martinezferran',
-    'sergisoleMD',
-    'Benayadachraf',
-    'fansjavimiguel', 'EsportsRAC1'
-]
+with open('handles.json', 'r') as f:
+  follow_handles = json.load(f)['handles']
